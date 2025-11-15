db1b836943d12a386acd2aed797885a1d38c323d
Chun Man Wong <wroy123@gmail.com>
2025-11-15 20:40:10 +0900

    completed 1 question

---
+++ Search/Triple_sum.py
@@ -0,0 +1,16 @@
+def triplets(a, b, c):
+    count, pa, pc = 0, 0, 0
+    a = sorted(set(a))
+    b = sorted(set(b))
+    c = sorted(set(c))
+
+    pa = pc = 0 
+    for val in b:
+        while pa < len(a) and val >= a[pa]:
+            pa += 1
+        while pc < len(c) and val >= c[pc]:
+            pc += 1
+        count += pa * pc
+    return count
+
+print(triplets([1,3,5], [2,3], [1,2,3]))
